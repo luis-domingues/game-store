406882bd9334def12389af4cb3d1e0bab0b3386b
luis-domingues <luisdomingues117@gmail.com>
2024-07-18 11:00:56 -0300

    feat: add developers list, methods for adding, view and editing developers
    fix: handle invalid developer index to prevent erros when attempting to access or modify developers with invalid indices

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import com.gamestore.developer.Developer;
 import com.gamestore.game.Game;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static Developer[] developers = new Developer[100];
-    static int numDevelopers = 0;
+    static ArrayList<Developer> developers = new ArrayList<Developer>();
 
     public static void main (String[] args) {
         Scanner in = new Scanner(System.in);
@@ -21,12 +22,34 @@ public class Main {
                 System.out.println ("Developer name:");
                 String name = in.next();
                 Developer dev = new Developer(name);
-                developers[numDevelopers] = dev;
-                numDevelopers++;
+                developers.add(dev);
             }
             if (userOption == 2) {
-                for (int i = 0; i < numDevelopers; i++) {
-                    System.out.println ("\nDeveloper ID[" + i + "]: " + developers[i].getNameDeveloper());
+                for (int i = 0; i < developers.size(); i++) {
+                    System.out.println ("\nDeveloper ID[" + i + "]: " + developers.get(i).getNameDeveloper());
+                }
+            }
+            if (userOption == 3) {
+                System.out.println("What is the Developer ID that will be removed?");
+                int i = in.nextInt();
+                if (i < developers.size()) {
+                    developers.remove(i);
+                    System.out.println("Success! Developer removed.");
+                } else {
+                    System.out.println("Invalid index! Type it again.");
+                }
+            }
+            if (userOption == 4) {
+                System.out.println("Which Developer ID will be changed?");
+                int i = in.nextInt();
+                if (i < developers.size()) {
+                    Developer dev = developers.get(i);
+                    System.out.println("Enter new developer name?");
+                    String name = in.next();
+                    dev.setNameDeveloper(name);
+                    System.out.println("Success! Developer name changed to " + dev.getNameDeveloper());
+                } else {
+                    System.out.println("Invalid index! Type it again.");
                 }
             }
         }
@@ -36,9 +59,11 @@ public class Main {
 
     static void printMenu() {
         System.out.println ("\n_________________________________________________________________________________________\n");
-        System.out.println ("[0].   Exit.");
-        System.out.println("[1].    Developer, register by selecting here.");
-        System.out.println("[2].    Registered developers.");
+        System.out.println ("[0].    Exit.");
+        System.out.println ("[1].    Press here to register a developer.");
+        System.out.println ("[2].    Press here to list listed developers.");
+        System.out.println ("[3].    Press here to remove a listed developer.");
+        System.out.println ("[4].    Press here to change a listed developer.");
         System.out.println ("Search...");
     }
 }
