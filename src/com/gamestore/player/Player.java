9e52556f0f7401b52622b9232e0e651c636267f8
luis-domingues <luisdomingues117@gmail.com>
2024-07-18 09:58:49 -0300

    feat: add attributes, constructor and attribute getter and setter methods

--- src/com/gamestore/player/Player.java
+++ src/com/gamestore/player/Player.java
@@ -1,4 +1,39 @@
 package com.gamestore.player;
+import com.gamestore.game.Game;
 
 public class Player {
+    private String username;
+    private String email;
+    private Game[] games;
+
+    public Player (String username, String email, Game[] games) {
+        super();
+        this.username = username;
+        this.email = email;
+        this.games = games;
+    }
+
+    public String getUsername () {
+        return username;
+    }
+
+    public void setUsername (String username) {
+        this.username = username;
+    }
+
+    public String getEmail () {
+        return email;
+    }
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+
+    public Game[] getGames () {
+        return games;
+    }
+
+    public void setGames (Game[] games) {
+        this.games = games;
+    }
 }
