9b4570cfe5e490a1a466d5d9c2c369255de465da
luis-domingues <luisdomingues117@gmail.com>
2024-07-23 10:43:43 -0300

    feat: Add User class and create friends attribute in Player class

---
+++ src/com/gamestore/user/User.java
@@ -0,0 +1,42 @@
+package com.gamestore.user;
+
+import com.gamestore.game.Game;
+
+import java.util.ArrayList;
+
+public class User {
+    private String username;
+    private String email;
+    private ArrayList<Game> games;
+
+    public User(String username, String email) {
+        super();
+        this.username = username;
+        this.email = email;
+        this.games = new ArrayList<Game>();
+    }
+
+    public String getUsername () {
+        return username;
+    }
+
+    public void setUsername (String username) {
+        this.username = username;
+    }
+
+    public String getEmail () {
+        return email;
+    }
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Game> getGames () {
+        return games;
+    }
+
+    public void setGames (ArrayList<Game> games) {
+        this.games = games;
+    }
+}
